b5e43c52940ec00fb61385f4d38a5c47034dfc05
jchavezk <jchavezk@cpcom102macm36.care.asu.edu>
2015-04-01 17:57:23 -0700

    Added AppointmentView Controller.

---
+++ ScheduleMe/AppointmentsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  AppointmentsTableViewController.h
+//  ScheduleMe
+//
+//  Created by jchavezk on 4/1/15.
+//  Copyright (c) 2015 JCKortlang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppointmentsTableViewController : UITableViewController
+
+@property NSDate* selectedDate;
+
+@end
