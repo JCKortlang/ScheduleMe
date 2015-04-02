c2bdcf54936ee922a51371fdef473e31adfd31a7
jchavezk <jchavezk@cpcom102macm36.care.asu.edu>
2015-04-01 17:57:45 -0700

    Added AppointmentManager.

---
+++ ScheduleMe/AppointmentManager.h
@@ -0,0 +1,13 @@
+//
+//  AppointmentManager.h
+//  ScheduleMe
+//
+//  Created by jchavezk on 4/1/15.
+//  Copyright (c) 2015 JCKortlang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppointmentManager : NSObject
+
+@end
---
+++ ScheduleMe/AppointmentManager.m
@@ -0,0 +1,13 @@
+//
+//  AppointmentManager.m
+//  ScheduleMe
+//
+//  Created by jchavezk on 4/1/15.
+//  Copyright (c) 2015 JCKortlang. All rights reserved.
+//
+
+#import "AppointmentManager.h"
+
+@implementation AppointmentManager
+
+@end
